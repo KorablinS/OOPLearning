aea574981e93545ba6c3e943b90d4ccb6b7c6ac3
KorablinS <korablin198421@gmail.com>
2022-07-28 19:21:45 +0300

    Init of project

--- src/ru/synergy/constructorTest/Main.java
+++ src/ru/synergy/constructorTest/Main.java
@@ -4,30 +4,43 @@ import java.util.Enumeration;
 
 public class Main {
     public static void main(String[] args) {
-        Fraction fr = new Fraction(1,2);
+        Fraction fr = new Fraction(1, 2);
         fr.add1ToNumerator().add1ToNumerator().add1ToNumerator().add1ToNumerator().add1ToNumerator();
     }
 
 }
-    class Fraction {
+
+class Fraction {
     private int numerator;
     private int denominator;
 
     public Fraction(int numerator, int denominator) {
-        if(denominator == 0){
+        if (denominator == 0) {
             System.out.println("Denominator can't be zero");
             return;
 
         }
-        this.numerator =numerator;
+        this.numerator = numerator;
         this.denominator = denominator;
     }
-        public Fraction add1ToNumerator(){
+
+    public Fraction add1ToNumerator() {
 
         this.numerator++;
         return this;
-        }
-        public Fraction getObjetCurrent(){return this;}
-
-
     }
+
+    public Fraction getObjetCurrent() {
+        return this;
+    }
+
+
+}
+
+class Person {
+    private String firstName;
+    private String seconName;
+    private char gender;  // m-male,  f-female
+
+
+}
